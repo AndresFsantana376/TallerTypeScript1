1e77053a929a90b03428d20747c7763dbc30cb45
AndresFsantana376 <af.santana@uniandes.edu.co>
2025-11-12 18:02:53 -0500

    Taller TypeScript 1 completo

--- dist/data.js
+++ dist/data.js
@@ -1,8 +1,9 @@
-import { Serie } from './Serie.js';
+import { Serie } from "./serie.js";
 export const series = [
-    new Serie(1, "Breaking Bad", "AMC", 5, "Un profesor de química con cáncer se convierte en fabricante de metanfetamina.", "https://www.amc.com/shows/breaking-bad", "https://upload.wikimedia.org/wikipedia/en/6/61/Breaking_Bad_title_card.png"),
-    new Serie(2, "Game of Thrones", "HBO", 8, "Nueve familias nobles luchan por el control del Trono de Hierro en los Siete Reinos.", "https://www.hbo.com/game-of-thrones", "https://upload.wikimedia.org/wikipedia/en/d/d8/Game_of_Thrones_title_card.jpg"),
-    new Serie(3, "Stranger Things", "Netflix", 4, "Un grupo de niños descubre fenómenos sobrenaturales en su pequeña ciudad.", "https://www.netflix.com/title/80057281", "https://upload.wikimedia.org/wikipedia/en/f/f7/Stranger_Things_season_4.jpg"),
-    new Serie(4, "The Mandalorian", "Disney+", 3, "Un cazarrecompensas solitario se aventura por los confines de la galaxia tras la caída del Imperio.", "https://www.disneyplus.com/series/the-mandalorian/3jLIGMDYINqD", "https://upload.wikimedia.org/wikipedia/en/3/3d/The_Mandalorian_poster.jpg"),
-    new Serie(5, "The Boys", "Amazon Prime Video", 4, "Un grupo de vigilantes lucha contra superhéroes corruptos respaldados por una corporación poderosa.", "https://www.primevideo.com/detail/The-Boys/0G9QX3ZLPX5R8JZTK3MWJZPE6V", "https://upload.wikimedia.org/wikipedia/en/7/7e/The_Boys_Title_Card.jpg")
+    new Serie(1, "Breaking Bad", "AMC", 5),
+    new Serie(2, "Orange Is the New Black", "Netflix", 6),
+    new Serie(3, "Game of Thrones", "HBO", 7),
+    new Serie(4, "The Big Bang Theory", "CBS", 12),
+    new Serie(5, "Sherlock", "BBC", 4),
+    new Serie(6, "A Very English Scandal", "BBC", 2),
 ];
--- main.ts
+++ main.ts
@@ -3,35 +3,20 @@ import { series } from './data.js';
 
 const seriesTbody: HTMLElement = document.getElementById("series")!;
 const avgElement: HTMLElement = document.getElementById("average-seasons")!;
-const detailContainer: HTMLElement = document.getElementById("series-detail")!;
 
 renderSeriesInTable(series);
 avgElement.innerHTML = `Promedio de temporadas: ${getAverageSeasons(series).toFixed(2)}`;
 
 function renderSeriesInTable(series: Serie[]): void {
   series.forEach(s => {
-    let tr = document.createElement("tr");
+    const tr = document.createElement("tr");
     tr.innerHTML = `<td>${s.id}</td><td>${s.name}</td><td>${s.channel}</td><td>${s.seasons}</td>`;
-    tr.addEventListener("click", () => showSeriesDetail(s));
     seriesTbody.appendChild(tr);
   });
 }
 
 function getAverageSeasons(series: Serie[]): number {
-  let totalSeasons = 0;
-  series.forEach(s => totalSeasons += s.seasons);
-  return totalSeasons / series.length;
-}
-
-function showSeriesDetail(serie: Serie): void {
-  detailContainer.innerHTML = `
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="${serie.image}" alt="${serie.name}">
-      <div class="card-body">
-        <h5 class="card-title">${serie.name}</h5>
-        <p class="card-text">${serie.description}</p>
-        <a href="${serie.webpage}" target="_blank" class="btn btn-primary">Ver más</a>
-      </div>
-    </div>
-  `;
+  if (series.length === 0) return 0;
+  const total = series.reduce((acc, s) => acc + s.seasons, 0);
+  return total / series.length;
 }
